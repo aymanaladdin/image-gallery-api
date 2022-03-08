de36c2b0719cba63e425e477d8d83033fb915bd7
Ayman Aladdin <a.alaaeldin@breadfast.com>
2022-03-08 22:27:34 +0200

    fix list pagination

--- src/api/topics/topic.repository.ts
+++ src/api/topics/topic.repository.ts
@@ -1,45 +1,16 @@
-import data from '../../util/data';
 import { IFindOption } from '../../util/types';
-import { Topic } from './types';
-
-const topicCache: { data: Topic[] } = {
-  data: [],
-};
-
-const initCache = () => {
-  const topicMap = new Map<string, { url: string }[]>();
-
-  data.images.forEach((image) => {
-    image.topics?.forEach((topic) => {
-      if (topicMap.has(topic)) {
-        const collages = topicMap.get(topic);
-
-        if (collages && collages.length < 4) {
-          collages.push({ url: image.url });
-        }
-      } else {
-        const collages = [{ url: image.url }];
-        topicMap.set(topic, collages);
-      }
-    });
-  });
-
-  const topics: Topic[] = [];
-
-  topicMap.forEach((collages, topic) => {
-    topics.push({ name: topic, collages });
-  });
-
-  topicCache.data = topics.sort();
-};
-
-initCache();
+import topicCache from '../../util/data/topic-cache';
+import { getSliceBoundaries } from '../../util/helpers';
 
 const topicRepository = {
-  find: (options?: IFindOption) => ({
-    data: topicCache.data.slice(options?.offset ?? 0, (options?.limit ?? 10) + 1),
-    totalCount: topicCache.data.length,
-  }),
+  find: (options?: IFindOption) => {
+    const [start, end] = getSliceBoundaries(options?.limit ?? 10, options?.offset ?? 0);
+
+    return {
+      data: topicCache.data.slice(start, end),
+      totalCount: topicCache.data.length,
+    };
+  },
 
 };
 
