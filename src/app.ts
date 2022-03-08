ae8072e37e398a5195b86a5d22f03d199d70d282
Ayman Aladdin <a.alaaeldin@breadfast.com>
2022-03-08 22:28:04 +0200

    add swagger ui for API docs

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,12 @@ import helmet from 'helmet';
 import methodOverride from 'method-override';
 import { INTERNAL_SERVER_ERROR, NOT_FOUND } from 'http-status';
 
-import { API_VERSION } from './util/constants';
+import { API_VERSION, HOST_NAME } from './util/constants';
 import appRoutes from './api/routes';
 
 const app = express();
 
-app.use(morgan('combined'));
+app.use(morgan('common'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,19 +24,24 @@ app.use(helmet());
 app.use(cors());
 app.use(methodOverride());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', 'docs')));
-}
+console.log(path.join(__dirname, '..', 'docs'));
+
+app.use(express.static(path.join(__dirname, '..', 'docs')));
 
 // mount api v1 routes
 app.use(API_VERSION, appRoutes);
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', HOST_NAME);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // catch 404 and forward to error handler
 app.all('*', (_, res) => {
   res.status(NOT_FOUND).send('<h1>404! Page not found</h1>');
 });
 
-// error handler, send stacktrace only during development
 app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
   if (res.headersSent) return next(err);
 
